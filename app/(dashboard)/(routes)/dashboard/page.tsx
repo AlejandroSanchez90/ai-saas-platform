9be084df57947fb491be0f7df99cc5917078b8bb
AlejandroSanchez90 <contact@alejandrosanchez.dev>
2023-08-21 00:13:14 -0500

    File Structure
    Trying that group folder for nextjs

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,6 @@
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+
+export default function DashboardPage() {
+  return <p>Dashboard Page (Protected)</p>;
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+function LandingPage({}: Props) {
+  return <div>LandingPage (Unprotected)</div>;
+}
+
+export default LandingPage;
