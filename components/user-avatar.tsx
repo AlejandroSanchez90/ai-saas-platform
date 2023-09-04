39c82fd4844b93101e6cdca10f558e0c630aabbd
AlejandroSanchez90 <contact@alejandrosanchez.dev>
2023-09-04 14:33:05 -0500

    Conversation AI Api
    Connected to OpenAI api to ask questions

---
+++ components/user-avatar.tsx
@@ -0,0 +1,20 @@
+import { useUser } from '@clerk/nextjs';
+import React from 'react';
+import { AvatarImage, Avatar, AvatarFallback } from '@/components/ui/avatar';
+
+type Props = {};
+
+function UserAvatar({}: Props) {
+  const { user } = useUser();
+  return (
+    <Avatar className='h-8 w-8'>
+      <AvatarImage src={user?.imageUrl} />
+      <AvatarFallback>
+        {user?.firstName?.charAt(0)}
+        {user?.lastName?.charAt(0)}
+      </AvatarFallback>
+    </Avatar>
+  );
+}
+
+export default UserAvatar;
